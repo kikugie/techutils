9d8345c306e96f52e59c238c67af34a00d027f78
Kikugie <kikugie@duck.com>
2023-01-29 20:45:24 +0100

    Use full name

--- src/main/java/me/kikugie/techutils/Reference.java
+++ src/main/java/me/kikugie/techutils/Reference.java
@@ -2,6 +2,6 @@ package me.kikugie.techutils;
 
 public class Reference {
     public static final String MOD_ID = "tech-utils";
-    public static final String MOD_NAME = "Technical Utils";
+    public static final String MOD_NAME = "Technical Utilities";
     public static final String MOD_VERSION = "0.1.0";
 }
--- src/main/java/me/kikugie/techutils/TechUtilsMod.java
+++ src/main/java/me/kikugie/techutils/TechUtilsMod.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TechUtilsMod implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("tech-utils");
+    public static final Logger LOGGER = LoggerFactory.getLogger(Reference.MOD_ID);
 
     @Override
     public void onInitialize() {
