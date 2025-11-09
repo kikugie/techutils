54d0e11529810cbf139484f485498a22d48c55cf
Velizar <31567122+VelizarBG@users.noreply.github.com>
2025-11-09 04:05:29 +0200

    Make the inventory overlay's click handling significantly less eager

--- src/main/java/dev/kikugie/techutils/mixin/containerscan/ClientPlayerInteractionManagerMixin.java
+++ src/main/java/dev/kikugie/techutils/mixin/containerscan/ClientPlayerInteractionManagerMixin.java
@@ -1,20 +1,26 @@
 package dev.kikugie.techutils.mixin.containerscan;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.kikugie.techutils.feature.containerscan.verifier.InventoryOverlay;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayerInteractionManager.class)
 public class ClientPlayerInteractionManagerMixin {
-	@Inject(method = "interactBlock", at = @At(value = "RETURN", ordinal = 1))
-	private void recordContainer(ClientPlayerEntity player, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
-		InventoryOverlay.onContainerClick(hitResult);
+	@Definition(id = "ActionResult", type = ActionResult.class)
+	@Definition(id = "getValue", method = "Lorg/apache/commons/lang3/mutable/MutableObject;getValue()Ljava/lang/Object;")
+	@Expression("return @((ActionResult) ?.getValue())")
+	@ModifyExpressionValue(method = "interactBlock", at = @At("MIXINEXTRAS:EXPRESSION"))
+	private ActionResult recordContainer(ActionResult original, @Local(argsOnly = true) BlockHitResult hitResult) {
+		if (original.isAccepted()) {
+			InventoryOverlay.onContainerClick(hitResult);
+		}
+		return original;
 	}
 }
